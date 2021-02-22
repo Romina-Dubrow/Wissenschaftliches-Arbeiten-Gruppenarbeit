51b2d979eec533b98fcb66d5e029ade9b72bf83d
Romina <78644135+Romina-Dubrow@users.noreply.github.com>
2021-02-22 17:58:16 +0100

    Update Aufgabe 4.R
    Ich habe mal meine Funktion angewendet. Ich hoffe man kann alles ganz gut erkennen. Ich war mir nicht so sicher was ich verwenden soll, weil ich ja eine Funktion erstellen sollte, die mindestens ordinale Variablen verwenden soll, aber ich habe nur die metrischen Variablen, also das Interesse an Programmieren und Mathematik benutzt. Alles andere ergibt meiner Meinung nach nicht so viel Sinn. Aber wenn euch da noch was einfällt kann ich das auch noch machen :)

--- Aufgabe 4.R
+++ Aufgabe 4.R
@@ -1,2 +1,34 @@
 ### Analyse des Datensatzes 
 
+Daten <- read.csv("Datensatz.csv")
+
+### Kategorisierung der Daten mithilfe von Quantilen (Funktion e) )
+
+## Studienfach und Interesse:
+Fach<-split(Daten,Daten$Studienfach)
+
+for (i in 1:4) {
+  print("Studienfach")
+  print(Fach[[i]]$Studienfach[1])
+  print("Mathematikinteresse")
+  e(Fach[[i]]$Intersse.an.Mathematik)
+  print("Studienfach")
+  print(Fach[[i]]$Studienfach[1])
+  print("Programmierinteresse")
+  e(Fach[[i]]$Intersse.an.Programmieren)
+}
+
+## Mathe LK (ja/nein) und Interesse:
+LK <- split(Daten,Daten$Mathe.LK)
+for (i in 1:2) {
+  print("Mathe LK?")
+  print(LK[[i]]$Mathe.LK[1])
+  print("Mathematikinteresse")
+  e(LK[[i]]$Intersse.an.Mathematik)
+  print("Mathe LK?")
+  print(LK[[i]]$Mathe.LK[1])
+  print("Programmierinteresse")
+  e(LK[[i]]$Intersse.an.Programmieren)
+}
+
+
