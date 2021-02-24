6d83ad5021134c53694335b94fe373708be82139
lucasauer <74325375+lucasauer@users.noreply.github.com>
2021-02-24 10:48:55 +0100

    Update and rename Aufgabe1-GitHub.R to Aufgabe1.R
    Hab mal den Dateinamen geändert und die Dokumentation ein bisschen angepasst

--- Aufgabe1.R
+++ Aufgabe1.R
@@ -1,17 +1,31 @@
 ## Aufgabe 1
 
-sim <- data.frame(ID = 1:100)  ## Eine ID-Spalte(?) vllt. reicht rownames
+## Erstellen eines Dataframes mit der Spalte ID fuer 100 Beobachtungen
+
+sim <- data.frame(ID = 1:100) 
+
 
 # a)
 # Alter
 
-set.seed(0957)  ## Fuer Nachvollziehbarkeit
-Alter <- round(rnorm(100, mean = 25, sd = 2))  ## gerundet
+## Fuer Nachvollziehbarkeit wird fuer jede Teilaufgabe ein seed verwendet
+
+set.seed(0957) 
+
+## Simulation des Alters mittels Zufallszahlen der Normalverteilung. Zusaetlich wird das Alter
+## auf einen ganzzahligen Wert gerundet.
+
+Alter <- round(rnorm(100, mean = 25, sd = 2)) 
+
+## Hinzufuegen zum Dataframe in neuer Spalte
+
 sim$Alter <- Alter
 
 
 # b)
 # Studienfach
+
+## Erstellen eines Vektors mit allen moeglichen Faechern
 
 faecher <- c("Statistik", "Data Science", "Informatik", "Mathematik")
 
@@ -25,14 +39,19 @@ sim$Studienfach <- as.factor(sample(faecher, 100, prob = c(0.35, 0.35, 0.2, 0.1)
 # c)
 # Interesse an Mathematik
 
+## Auspraegung des Interesse (wobei 1 gering und 7 sehr stark)
+
 interesse <- 1:7
+
+## Erstellung von Wahrscheinlichkeitsvektoren zu den einzelnen Faechern
+
 pm_stat <- c(0, 0.05, 0.1, 0.2, 0.35, 0.2, 0.1)  ## W'keits-Vektor Statistik
 pm_data <- c(0.05, 0.15, 0.2, 0.25, 0.2, 0.15, 0)  ## W'keits-Vektor Data Science
 pm_info <- c(0.1, 0.15, 0.3, 0.2, 0.15, 0.1, 0) ## ... Informatik
 pm_mathe <- c(0, 0, 0.05, 0.15, 0.3, 0.3, 0.2) ## ... Mathematik
 
-## Schachtelung mehrerer ifelse-Konstruktionen und anschliessendes zufaelliges Ziehen
-## nach Fach und dazugehoerigen W'keitsvektoren
+## Schachtelung mehrerer ifelse-Konstruktionen zur Simulation des Interesses an Mathematik
+## und anschliessendes zufaelliges Ziehen nach Fach und dazugehoerigen W'keitsvektoren
 
 set.seed(1015)
 IntM <- ifelse(sim$Studienfach == "Statistik", 
@@ -48,13 +67,15 @@ sim$"Intersse an Mathematik" <- IntM
 # d)
 # Interesse an Programmieren
 
+## Wahrscheinlichkeitsvektoren analog zu c)
+
 pi_stat <- c(0, 0.1, 0.2, 0.4, 0.2, 0.1, 0)  ## W'keits-Vektor Statistik
 pi_data <- c(0, 0.05, 0.1, 0.2, 0.3, 0.2, 0.15)  ## W'keits-Vektor Data Science
 pi_info <- c(0, 0, 0.05, 0.1, 0.25, 0.3, 0.3) ## ... Informatik
 pi_mathe <- c(0.2, 0.25, 0.25, 0.15, 0.1, 0.05, 0) ## ... Mathematik
 
 ## Schachtelung mehrerer ifelse-Konstruktionen und anschliessendes zufaelliges Ziehen
-## nach Fach und dazugehoerigen W'keitsvektoren
+## nach Fach und dazugehoerigen W'keitsvektoren. Vektor interesse aus c)
 
 set.seed(1022)
 IntP <- ifelse(sim$Studienfach == "Statistik", 
@@ -86,3 +107,9 @@ MLK <-ifelse(sim$`Intersse an Mathematik` > 5, "Ja",
               sample(c("Ja", "Nein"), sum(sim$Studienfach == "Informatik" & sim$`Intersse an Mathematik` <= 5),
                      prob = c(0.1, 0.9), replace = T)))))
 sim$"Mathe-LK" <- as.factor(MLK)
+
+
+
+## Exportieren des simulierten Datensatzes als csv-Datei
+
+write.csv(sim, "Datensatz.csv", row.names = FALSE)
